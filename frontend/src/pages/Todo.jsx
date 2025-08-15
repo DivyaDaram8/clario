2752790e3afc5abaa79f2905f38ac1d08e4bbdde
DivyaDaram8 <divyadaram0416@gmail.com>
2025-08-15 23:45:35 +0530

    deleted NoteForm

--- frontend/src/pages/Todo.jsx
+++ frontend/src/pages/Todo.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import NavbarLeft from "../layout/NavbarLeft";
+import NavbarTop from "../layout/NavbarTop";
 
 function Todo() {
     return (
         <div>
+            <NavbarLeft />
+            <NavbarTop />
             <h1 className="text-4xl font-bold text-center text-blue-600 my-4">
                 My To-Do List 
             </h1>
