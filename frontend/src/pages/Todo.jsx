76385e6185dc4779403aab69ee4c21c1c07bd61a
DivyaDaram8 <divyadaram0416@gmail.com>
2025-08-16 10:34:50 +0530

    working wiht todo

--- frontend/src/pages/Todo.jsx
+++ frontend/src/pages/Todo.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import NavbarLeft from "../layout/NavbarLeft";
+import NavbarTop from "../layout/NavbarTop";
 
 function Todo() {
     return (
-        <div>
-            <h1 className="text-4xl font-bold text-center text-blue-600 my-4">
+        <>
+            <NavbarLeft />
+            <NavbarTop />
+            <h1 className="text-4xl font-bold text-center text-blue-600 my-4 mt-26">
                 My To-Do List 
             </h1>
-        </div>
+        </>
     )
 };
 
