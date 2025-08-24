9dd72afde4fd546eae2b0ee27f2fafd559010a82
DivyaDaram8 <divyadaram0416@gmail.com>
2025-08-24 22:42:58 +0530

    deleted journal frontend

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -8,19 +8,11 @@ export default function Journal() {
   );
 
   return (
-    <div className="h-screen w-full flex flex-col">
-      {/* Top navbar */}
-      <div className="h-14 fixed top-0 left-0 right-0 z-10">
-        <NavbarTop />
-      </div>
-
-      {/* Body */}
-      <div className="flex flex-1 pt-14">
-        {/* Left navbar */}
-        <div className="w-56 fixed top-14 bottom-0 left-0 z-10">
-          <NavbarLeft />
-        </div>
-        <div>journal</div>
+    <div>
+      <NavbarTop />
+      <NavbarLeft />
+      <div className="main-content">
+        <h1>Journal</h1>
       </div>
     </div>
   );
