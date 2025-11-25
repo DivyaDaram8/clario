44561c7ed86c239c403bc8e28d9a1db6d821f199
Lakshmi Divya Daram <divyadaram0416@gmail.com>
2025-11-25 23:54:49 +0530

    Changed name

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -49,7 +49,7 @@ export default function App() {
           transition={{ duration: 1 }}
           className="text-6xl md:text-8xl font-extrabold tracking-tight text-white drop-shadow-[0_0_50px_rgba(255,255,255,0.25)]"
         >
-          CLarIo
+          cLarIo
         </motion.h1>
 
         <motion.p
